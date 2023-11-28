bb412903ebcb5430e26d76fd28ce4f97682116be
muhamedshijas <shijushijas157@gmail.com>
2023-11-28 10:33:57 +0530

    chakra ui updated

--- Client/src/Components/User/Login/Login.js
+++ Client/src/Components/User/Login/Login.js
@@ -1,41 +1,63 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-import loginImage from '../../../assets/images/second-image.png'
-import './Login.css'
-import { MDBInput } from 'mdb-react-ui-kit'
+import React, { useState } from "react";
+import { FaUserLock } from "react-icons/fa";
+import axios from "axios";
+import loginImage from "../../../assets/images/second-image.png";
+import { IoIosMail } from "react-icons/io";
+import "./Login.css";
+import { MDBInput } from "mdb-react-ui-kit";
+import {
+  Box,
+  Button,
+  FormControl,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Text
+} from "@chakra-ui/react";
 function Login() {
-  const [email,setEmail]=useState("")
-  const [password,setPassword]=useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  async function handleSubmit(e){
-    e.preventDefault()
-    let {data}= await axios.post('/user/login',{email,password})
-
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let { data } = await axios.post("/user/login", { email, password });
   }
   return (
     <div>
-      <section className='login-main'>
-      <div className="login">
-      <div className="login-image">
-      <img src={loginImage} alt="" srcset="" />
-      </div>
-      <div className="login-form">
-      <h3>Student Login</h3>
-      <form action="">
-      <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" value={email} onChange={(e)=>setEmail(e.target.value)} />
-      <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"  value={password} onChange={(e)=>setPassword(e.target.value)}/>
-      <div className="forget w-100">
-      <p className='text-primary'> Forget Password</p>
-      </div>
-      <button onClick={handleSubmit}>Submit</button>
-      </form>
-      <p>New User Sign up here</p>
-      
-      </div>
-      </div>
+      <section className="login-main">
+        <div className="login">
+          <div className="login-image">
+            <img src={loginImage} alt="" srcset="" />
+          </div>
+          <div className="login-form">
+            <Box w="100%" h="60%" display="flex" flexDirection="column" justifyContent="around" alignItems="center" >
+            <Text>
+            <Heading>User Login</Heading>
+            </Text>
+              <FormControl w="80%" display="flex" flexDirection="column" justifyContent="space-around" h="80%">
+                <InputGroup>
+                  <Input variant="flushed" placeholder="Email" />
+                  <InputLeftElement pointerEvents="none">
+                    <IoIosMail />
+                  </InputLeftElement>
+                </InputGroup>
+
+                <InputGroup>
+                  <Input variant="flushed" placeholder="Password" />
+                  <InputLeftElement pointerEvents="none">
+                    <FaUserLock />
+                  </InputLeftElement>
+                </InputGroup>
+
+                <Button w="100%" colorScheme="green">Submit</Button>
+              </FormControl>
+            </Box>
+          </div>
+        </div>
       </section>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- Client/src/index.js
+++ Client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from "react-dom/client";
+
+import { ChakraProvider } from '@chakra-ui/react'
 import './index.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import App from './App';
@@ -12,10 +14,15 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+  <ChakraProvider>
   <BrowserRouter>
   <App />
   </BrowserRouter>  
+  </ChakraProvider>
   </React.StrictMode>
+
+
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
