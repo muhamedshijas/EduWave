34f763e87d57ffb15c5a084891ac8a70c8734fd1
muhamedshijas <shijushijas157@gmail.com>
2023-11-29 11:19:30 +0530

    user ui updated

--- Client/src/Components/User/Login/Login.js
+++ Client/src/Components/User/Login/Login.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { FaUserLock } from "react-icons/fa";
 import axios from "axios";
-import loginImage from "../../../assets/images/second-image.png";
+import loginImage from "../../../assets/images/loginImage.png";
 import { IoIosMail } from "react-icons/io";
 import "./Login.css";
-import { MDBInput } from "mdb-react-ui-kit";
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
 import {
   Box,
   Button,
@@ -13,12 +14,17 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
-  Text
+  InputRightElement,
+  Link,
+  Text,
 } from "@chakra-ui/react";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [show, setShow] = useState(false);
+  function handleShow() {
+    setShow(!show);
+  }
   async function handleSubmit(e) {
     e.preventDefault();
     let { data } = await axios.post("/user/login", { email, password });
@@ -31,27 +37,69 @@ function Login() {
             <img src={loginImage} alt="" srcset="" />
           </div>
           <div className="login-form">
-            <Box w="100%" h="60%" display="flex" flexDirection="column" justifyContent="around" alignItems="center" >
-            <Text>
-            <Heading>User Login</Heading>
-            </Text>
-              <FormControl w="80%" display="flex" flexDirection="column" justifyContent="space-around" h="80%">
+            <Box
+              w="100%"
+              h="70%"
+              display="flex"
+              flexDirection="column"
+              justifyContent="around"
+              alignItems="center"
+            >
+              <Text>
+                <Heading>User Login</Heading>
+              </Text>
+              <FormControl
+                w="80%"
+                display="flex"
+                flexDirection="column"
+                justifyContent="space-around"
+                h="80%"
+              >
                 <InputGroup>
-                  <Input variant="flushed" placeholder="Email" />
+                  <Input
+                    variant="flushed"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
                   <InputLeftElement pointerEvents="none">
                     <IoIosMail />
                   </InputLeftElement>
                 </InputGroup>
 
                 <InputGroup>
-                  <Input variant="flushed" placeholder="Password" />
+                  <Input
+                    variant="flushed"
+                    placeholder="Password"
+                    type={show ? "text" : "password"}
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                  />
                   <InputLeftElement pointerEvents="none">
                     <FaUserLock />
                   </InputLeftElement>
+
+                  <InputRightElement onClick={handleShow}>
+                    {show ? <FaRegEyeSlash /> : <FaRegEye />}
+                  </InputRightElement>
                 </InputGroup>
 
-                <Button w="100%" colorScheme="green">Submit</Button>
+                <Button w="100%" colorScheme="green" onClick={handleSubmit}>
+                  Submit
+                </Button>
               </FormControl>
+
+              <Text as="b">
+                Dont you have an acount
+                <Link color="blue.500" href="/signup">
+                  {" "}
+                  Sign Up here
+                </Link>
+              </Text>
             </Box>
           </div>
         </div>
--- Client/src/Components/User/SignUp/SignUp.js
+++ Client/src/Components/User/SignUp/SignUp.js
@@ -1,23 +1,167 @@
-import React from 'react'
-import signUpImage from '../../../assets/images/main-image.png'
-import './SignUp.css'
+import React, { useState } from "react";
+import signUpImage from "../../../assets/images/main-image.png";
+import "./SignUp.css";
+import { IoIosMail } from "react-icons/io";
+import loginImage from "../../../assets/images/loginImage.png";
+import { FaUserLock } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { FaMobileButton } from "react-icons/fa6";
+import { GiConfirmed } from "react-icons/gi";
+import { MdOutlineFileOpen } from "react-icons/md";
+import {
+  Box,
+  Button,
+  FormControl,
+  Input,
+  Image,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Stack,
+  Heading,
+  Text,
+  FormErrorMessage,
+} from "@chakra-ui/react";
 function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState();
+  const [show, setShow] = useState(false);
+  const [cPassword, setCPassword] = useState("");
+  function handleShow() {
+    setShow(!show);
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let { data } = await axios.post("/user/login", { email, password });
+  }
+
+  const nameErr=name===""
+  const emailErr=name===""
+  const passwordErr=password!==cPassword
+  const phoneErr= phone===""
   return (
-    <div>
-      <section className='signup-main'>
-      <div className="signup">
-      <div className="signup-image">
-      <img src={signUpImage} alt="" srcset="" />
-      </div>
-      <div className="signup-form">
-      <h3>Student signup</h3>
-      <form action="">
-      </form>
-      </div>
-      </div>
-      </section>
-    </div>
-  )
+    <Box
+      w="100%"
+      h="100vh"
+      bg="blue"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box
+        h="60%"
+        p="4"
+        w="60%"
+        bg="#fff"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box h="100%" w="90%">
+          <Image src={loginImage} />
+        </Box>
+
+        <FormControl>
+          <Stack spacing={4}>
+            <Text display="flex" fontSize="4xl" justifyContent="space-between"  w="55%" alignItems="center">
+            <MdOutlineFileOpen style={{color:"blue"}} />
+              <Heading>User Sign Up</Heading>
+            </Text>
+            <InputGroup>
+              <Input
+                variant="flushed"
+                placeholder="Enter Your Name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+              <InputLeftElement pointerEvents="none">
+                <FaUser />
+              </InputLeftElement>
+             {
+              nameErr&& <FormErrorMessage>
+              Name is requierd
+              </FormErrorMessage>
+             }
+            </InputGroup>
+            <InputGroup>
+              <Input
+                variant="flushed"
+                placeholder=" Enter Your Email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <InputLeftElement pointerEvents="none">
+                <IoIosMail />
+              </InputLeftElement>
+            </InputGroup>
+            <InputGroup>
+              <Input
+                variant="flushed"
+                placeholder="Enter Your mobile No"
+                value={phone}
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                }}
+              />
+              <InputLeftElement pointerEvents="none">
+                <FaMobileButton />
+              </InputLeftElement>
+            </InputGroup>
+
+            <InputGroup>
+              <Input
+                variant="flushed"
+                placeholder="Password"
+                type={show ? "text" : "password"}
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <InputLeftElement pointerEvents="none">
+                <FaUserLock />
+              </InputLeftElement>
+
+              <InputRightElement onClick={handleShow}>
+                {show ? <FaRegEyeSlash /> : <FaRegEye />}
+              </InputRightElement>
+            </InputGroup>
+
+            <InputGroup>
+              <Input
+                variant="flushed"
+                placeholder="Password"
+                type={show ? "text" : "password"}
+                value={cPassword}
+                onChange={(e) => {
+                  setCPassword(e.target.value);
+                }}
+              />
+              <InputLeftElement pointerEvents="none">
+                <GiConfirmed />
+              </InputLeftElement>
+
+              <InputRightElement onClick={handleShow}>
+                {show ? <FaRegEyeSlash /> : <FaRegEye />}
+              </InputRightElement>
+            </InputGroup>
+
+            <Button w="100%" colorScheme="green" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </Stack>
+        </FormControl>
+      </Box>
+    </Box>
+  );
 }
 
-export default SignUp
+export default SignUp;
